4af1c4e3ea58d6d2fbfce48f3372d3b2f6a36f75
ecko9 <jean.ferstler@gmail.com>
2022-01-20 15:18:28 +0100

    design footer

--- src/components/navigation/Contact/index.jsx
+++ src/components/navigation/Contact/index.jsx
@@ -3,13 +3,24 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='Contact'>
+
       <h2>Contact</h2>
+
       <div className='contact-info'>
         <h3>Jessica Lyfoung - Photographe</h3>
-        <p>Montpellier</p>
-        <p>Instagram - JessNoodle</p>
-        <p>Facebook - JiJiNoodle</p>
+        <h4>Montpellier</h4>
+
+        <div className='networks'>
+          <p>JessNoodle</p>
+          <i className="fab fa-instagram"></i>
+        </div>
+
+        <div className='networks'>
+          <p>JiJiNoodle</p>
+          <i className="fab fa-facebook-square"></i>
+        </div>
       </div>
+
     </div>
   );
 };
--- src/components/navigation/Footer/index.jsx
+++ src/components/navigation/Footer/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Contact from '../Contact';
-import NavLogos from '../NavLogos';
 
 const Footer = () => {
   return (
     <div className='Footer'>
-      <NavLogos />
       <Contact />
-      <div className='FooterDesignBlock'></div>
     </div>
   );
 };
