a9385e5ef471a2bf1edb5234037c8195cc36aca8
ecko9 <jean.ferstler@gmail.com>
2022-01-20 21:39:27 +0100

    about page design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const About = () => {
   return (
     <div className='About'>
-      <h1>A propos</h1>
-      <p>Je pratique la photographie depuis ... Mon travail s'oriente sur ...</p>
+      <div className='description'>
+        <p>Je pratique la photographie depuis ... Mon travail s'oriente sur ...  Mon travail s'oriente sur ...  Mon travail s'oriente sur ...  Mon travail s'oriente sur ...  Mon travail s'oriente sur ...  Mon travail s'oriente sur ...</p>
+      </div>
     </div>
   );
 };
