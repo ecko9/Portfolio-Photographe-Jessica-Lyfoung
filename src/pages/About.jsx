f3c45bbba0079b3051605ad16e6a44d7a7154f5e
ecko9 <jean.ferstler@gmail.com>
2022-02-17 14:58:32 +0100

    remove useless code in index.html and correct some word syntax in About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,8 +18,8 @@ const About = () => {
             <p>Je vous présente ici mon univers et partage avec vous ce regard parfois dur, parfois tendre, de ce qui me touche au quotidien.</p>
             <p>Il y aura des mots, et surtout des <strong>images</strong>.</p>
             <p>La <strong>photographie</strong> m'a appris à me voir tel que je suis. J'ai compris à travers cette aventure que je pouvais m'amuser avec cette <strong>image</strong> et faire exister d'autres parts de moi même.</p>
-            <p>Cette pratique m'accompagne depuis ma tendre (plus ou moins) adolescence. Elle m'a aidé a voir mon corp et mon visage changer au fil du temps. Elle m'a aussi montré ce que j'apprécie le moins.</p>
-            <p>Aujourd'hui, je fais de cette pratique une alliée dans ma vie de tout les jours. Elle me ramène à l'essence brut de la vie. Elle redonne de la valeure aux instants présents et transmet l'espoir des jours à venir.</p>
+            <p>Cette pratique m'accompagne depuis ma tendre (plus ou moins) adolescence. Elle m'a aidé a voir mon corps et mon visage changer au fil du temps. Elle m'a aussi montré ce que j'apprécie le moins.</p>
+            <p>Aujourd'hui, je fais de cette pratique une alliée dans ma vie de tous les jours. Elle me ramène à l'essence brut de la vie. Elle redonne de la valeur aux instants présents et transmet l'espoir des jours à venir.</p>
             <p>Je vous souhaite un bon visionnage, et vous remercie de votre visite !</p>
           </div>
         </div>
